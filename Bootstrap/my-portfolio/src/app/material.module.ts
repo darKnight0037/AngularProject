d77f351bfe23b69c9ccc4558cf243f83686ac0b4
kumarankita28 <32949292+kumarankita28@users.noreply.github.com>
2019-12-10 23:09:01 +0530

    Delete material.module.ts

--- Bootstrap/my-portfolio/src/app/material.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import {MatCardModule, MatDividerModule, MatProgressBarModule} from '@angular/material';
-
-@NgModule({
-    imports: [MatCardModule ,MatDividerModule, MatProgressBarModule],
-    exports: [MatCardModule, MatDividerModule, MatProgressBarModule]
-})
-export class MaterialModule {}
